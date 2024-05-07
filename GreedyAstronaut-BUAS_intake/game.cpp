39b54f7cc59ed8c66b2a9c4ea053db44e659e949
Bram van Dijke <bvandijke03@gmail.com>
2024-05-07 05:20:16 +0200

    Switch statement ready for menu (States created)

--- GreedyAstronaut-BUAS_intake/game.cpp
+++ GreedyAstronaut-BUAS_intake/game.cpp
@@ -11,26 +11,60 @@ namespace Tmpl8
 	int screenX = ScreenWidth, screenY = ScreenHeight; // Get screen sizes
 	int px = screenX / 2 - 18, py = screenY / 2 - 18; // Place player in center of screen
 	
-	Sprite player(new Surface("assets/spaceship.tga"), 4);
-	Surface spaceBackground("assets/space_background.png");
+	Sprite player(new Surface("assets/spaceship.tga"), 4); // Player sprite
+	Surface spaceBackground("assets/space_background.png"); // Space background sprite
 
-	void Game::Init()
+	Sprite titleLogo(new Surface("assets/gameTitle.png"), 1); // Title logo sprite
+	Sprite playSprite(new Surface("assets/playButton.png"), 1); // Play button sprite
+	Sprite quitSprite(new Surface("assets/quitButton.png"), 1); // Quit button sprite
+
+	void Game::Init() //Runs when program initializes
+	{
+	}
+	void Game::Shutdown() // Runs when programs shuts down
 	{
 
 	}
-	void Game::Shutdown()
+	void Game::Tick(float deltaTime) // Runs every tick
 	{
+		switch (state)
+		{
+		case GameState::menuState:
+			MenuScreen(deltaTime);
+			break;
+		case GameState::gameState:
+			GameScreen(deltaTime);
+			break;
+		case GameState::scoreState:
+			ScoreScreen(deltaTime);
+			break;
+	}
 
 	}
-	void Game::Tick(float deltaTime)
+
+	void Game::MenuScreen(float deltaTime) // Runs when in menu screen
 	{
 		screen->Clear(0);
 		spaceBackground.CopyTo(screen, 0, 0); // Render background image
 		player.Draw(screen, px, py); // Draw player
 
+		screen->Line(mousex, 0, mousex, 511, 0xff0000); // for testing
+		screen->Line(0, mousey, 799, mousey, 0xff0000); // for testing
+
 		if (GetAsyncKeyState(VK_LEFT)) px--, player.SetFrame(3);
 		if (GetAsyncKeyState(VK_RIGHT)) px++, player.SetFrame(1);
 		if (GetAsyncKeyState(VK_UP)) py--, player.SetFrame(0);
 		if (GetAsyncKeyState(VK_DOWN)) py++, player.SetFrame(2);
+
+	}
+
+	void Game::GameScreen(float deltaTime) // Runs when in game screen
+	{
+
+	}
+
+	void Game::ScoreScreen(float deltaTime) // Runs when in score screen
+	{
+
 	}
 };
--- GreedyAstronaut-BUAS_intake/game.h
+++ GreedyAstronaut-BUAS_intake/game.h
@@ -3,7 +3,13 @@
 namespace Tmpl8 {
 
 class Surface;
+enum class GameState {
+	menuState,
+	gameState,
+	scoreState,
+};
 class Game
+
 {
 public:
 	void SetTarget( Surface* surface ) { screen = surface; }
@@ -12,11 +18,20 @@ public:
 	void Tick( float deltaTime );
 	void MouseUp( int button ) { /* implement if you want to detect mouse button presses */ }
 	void MouseDown( int button ) { /* implement if you want to detect mouse button presses */ }
-	void MouseMove( int x, int y ) { /* implement if you want to detect mouse movement */ }
+	void MouseMove(int x, int y) { mousex = x, mousey = y; } /*Mouse movement*/
 	void KeyUp( int key ) { /* implement if you want to handle keys */ }
 	void KeyDown( int key ) { /* implement if you want to handle keys */ }
+
+	void MenuScreen(float deltaTime); // function for menu screen
+	void GameScreen(float deltaTime); // function for game screen
+	void ScoreScreen(float deltaTime); // function for score screen
+
 private:
 	Surface* screen;
+	int mousex, mousey;
+	GameState state = GameState::menuState;
 };
+
+
 
 }; // namespace Tmpl8
